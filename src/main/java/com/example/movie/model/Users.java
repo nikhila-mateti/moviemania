f9e5b08dfa93e38a312798ecb45f869ca6a0164e
nikhila-mateti <s564202@nwmissouri.edu>
2024-05-11 18:52:43 -0500

    updated files

--- src/main/java/com/example/movie/model/Users.java
+++ src/main/java/com/example/movie/model/Users.java
@@ -1,21 +1,47 @@
 package com.example.movie.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 
 @Document(collection = "users")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Users {
+	
     @Id
     private String userId;
     private ArrayList<MovieIndividual> movies;
+    
+	public Users() {
+		super();
+	}
 
+	public Users(String userId, ArrayList<MovieIndividual> movies) {
+		super();
+		this.userId = userId;
+		this.movies = movies;
+	}
 
+	public ArrayList<MovieIndividual> getMovies() {
+		return movies;
+	}
+
+	public void setMovies(ArrayList<MovieIndividual> movies) {
+		this.movies = movies;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	@Override
+	public String toString() {
+		return "Users [userId=" + userId + ", movies=" + movies + "]";
+	}
+
+	
 }
